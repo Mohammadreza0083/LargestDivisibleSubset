018c5b767b9501bdf384ee7ffd22d275b214a41b
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-10 01:56:28 +0330

    Implement user input for LargestDivisibleSub method : in Program class

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Enter your array lenght :");
+            int InputLenght = Convert.ToInt32(Console.ReadLine());
+            int[] input = new int[InputLenght];
+            for (int i = 0; i < InputLenght; i++)
+            {
+                Console.WriteLine("Enter your num : ");
+                input[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            IList<int> result = LargestDivisibleSubset.LargestDivisibleSub(input);
+            Console.WriteLine("Largest Divisible Subset: [" + string.Join(", ", result) + "]");
         }
     }
 }
